a894c72d8eb2f05362654e780b080f82497ea2f6
gaponov <wmconstructor@gmail.com>
2023-01-20 12:00:39 +0200

    Add TimezoneValidateFilter. Add TimeUtils.

--- app/src/main/java/servlets/TimeServlet.java
+++ app/src/main/java/servlets/TimeServlet.java
@@ -5,22 +5,34 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import utils.TimeUtils;
 
 @WebServlet(value = "/time")
 public class TimeServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        SimpleDateFormat dateFormatLocal = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
-        dateFormatLocal.setTimeZone(TimeZone.getTimeZone("UTC"));
+  final private String DEFAULT_TIME_ZONE = "UTC";
 
-        resp.setContentType("text/html; charset=utf-8");
-        resp.getWriter().write(dateFormatLocal.format(new Date()));
-        resp.getWriter().close();
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    resp.setContentType("text/html; charset=utf-8");
+    resp.getWriter().write(getDateTimeFromUserQuery(req.getParameter("timezone")));
+    resp.getWriter().close();
+  }
+
+  private String getDateTimeFromUserQuery(String userTimeZone) {
+    ZoneId zoneId = TimeUtils.getTimeZone(userTimeZone);
+    if (zoneId == null) {
+      zoneId = ZoneId.of(DEFAULT_TIME_ZONE);
     }
+
+    Clock clock = Clock.system(zoneId);
+    return LocalDateTime.now(clock).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) +
+        " " + zoneId;
+  }
 }
